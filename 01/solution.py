fed2d25e910e8e740669585996f8c7344e83df24
tormobr <tormod.brandshoi@gmail.com>
2020-12-01 06:47:40 +0100

    made it pretty

--- 01/solution.py
+++ 01/solution.py
@@ -1,8 +1,23 @@
+import itertools
 from operator import mul
+from functools import reduce
 
+# Reads each number on each line in file and return list
 def read_lines(f):
     return [f(l.strip()) for l in open("input.txt")]
 
+# Finds all combinations and gets product of those where sum equals 2020
+def solve(r):
+    numbers = read_lines(int)
+    combs = itertools.combinations(numbers, r)
+    return [reduce(mul, c) for c in combs if sum(c) == 2020]
+
+if __name__ == "__main__":
+    print(solve(2))
+    print(solve(3))
+
+
+""" Less pretty edition
 
 def part1():
     numbers = read_lines(int)
@@ -19,6 +34,5 @@ def part2():
                 if n + n2 + n3 == 2020:
                     return n * n2 * n3
 
-if __name__ == "__main__":
-    print(part1())
-    print(part2())
+"""
+
