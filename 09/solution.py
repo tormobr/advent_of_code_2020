99e3b37a4c1190712d60a769aa3fdec672c69f08
tormobr <tormod.brandshoi@gmail.com>
2020-12-09 06:53:29 +0100

    some minor changes

--- 09/solution.py
+++ 09/solution.py
@@ -1,10 +1,4 @@
-import math
-from collections import deque, defaultdict
-from functools import reduce
-from operator import mul
 import itertools
-import numpy as np
-import re
 
 import sys
 sys.path.insert(0,'..')
@@ -13,17 +7,12 @@ from advent_lib import *
 # Part 1 solution : 
 def part_1():
     data = read_lines("input.txt", f=int)
-
-    res = 0
     step = 25
     for i in range(step+1, len(data)):
         nums = data[i-step:i]
         target = data[i]
-        if not any((n + n2 == target) for n, n2 in itertools.combinations(nums, 2)):
+        if not any((x + y == target) for x, y in itertools.combinations(nums, 2)):
             return target
-
-    return res
-
 
 # Part 2 solution : 
 def part_2():
@@ -32,19 +21,19 @@ def part_2():
     # i defines start of range
     for i, n in enumerate(data):
         current = n
-        # j defines end of range
-        for j, n2 in enumerate(data[i+1:]):
+        # j+i defines end of range
+        for j in range(i+1, len(data)):
+            current += data[j]
 
-            # Return results if match
+            # Return results if current sum match target
             if current == target:
-                hax = sorted(data[i:j+i+1])
-                return hax[-1] + hax[0]
+                result_range = data[i:j]
+                return min(result_range) + max(result_range)
 
             # if current sum is already over target, no need to continue
             if current > target:
                 break
 
-            current += n2
 
 
 if __name__ == "__main__":
