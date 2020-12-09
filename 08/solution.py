1652a08f7aa4f081705d609ec1a19ce25f8f830d
tormobr <tormod.brandshoi@gmail.com>
2020-12-09 02:27:01 +0100

    enhanced 08

--- 08/solution.py
+++ 08/solution.py
@@ -1,12 +1,3 @@
-from copy import deepcopy
-import math
-from collections import deque, defaultdict
-from functools import reduce
-from operator import mul
-import itertools
-import numpy as np
-import re
-
 import sys
 sys.path.insert(0,'..')
 from advent_lib import *
@@ -17,15 +8,24 @@ class solver:
         self.index = 0
         self.normal_ret = False
         self.input = self.read_input()
+        self.num_op_codes = len(self.input)
         self.op_funcs = {
             "acc": self.op_acc,
             "jmp": self.op_jmp,
             "nop": self.op_nop
         }
 
+    # Reads a input file and stores the data in array
     def read_input(self, filename="input.txt"):
-        return [(line.split()[0], int(line.split()[1])) for line in open(filename)]
+        data = []
+        with open(filename) as f:
+            for line in f:
+                op_code, val = line.split()
+                data.append([op_code, int(val)])
 
+        return data
+
+    # Functions to execute each operation
     def op_acc(self, v):
         self.acc += v
 
@@ -35,17 +35,16 @@ class solver:
     def op_nop(self, v):
         pass
 
-
-
-    def execute_op_codes(self, op_codes):
+    # Main function that loops through all operations and executes them
+    def execute_op_codes(self):
         # Reset the acc and the index
         self.acc = 0
         self.index = 0
         SEEN = set()
         while True:
-            op_code, val = op_codes[self.index]
+            op_code, val = self.input[self.index]
 
-            if self.index >= len(op_codes) -1:
+            if self.index >= self.num_op_codes - 1:
                 self.normal_term = True
                 break
             if self.index in SEEN:
@@ -57,21 +56,28 @@ class solver:
             self.op_funcs[op_code](val)
             self.index += 1
 
+    # Solves part 1 of day 8
     def part_1(self):
-        data = self.read_input()
-        self.execute_op_codes(self.input)
+        self.execute_op_codes()
         return self.acc
 
 
+    # Solves part 2 of day 8
     def part_2(self):
-        data = self.read_input()
-        replacements = [i for i, val in enumerate(data) if val[0] == "jmp"]
-        for r in replacements:
-            data[r] = ("nop", data[r][1])
-            self.execute_op_codes(data)
-            if self.normal_term:
-                return self.acc
-            data[r] = ("jmp", data[r][1])
+        switches = {"nop": "jmp", "jmp": "nop"} # possible switches
+        for key, value in switches.items():
+            # Get the indexes of the current switch item
+            replacements = [i for i, val in enumerate(self.input) if val[0] == key]
+            
+            # Switch one at a time and try to execute, and check normal termination
+            for r in replacements:
+                self.input[r][0] = value
+                self.execute_op_codes()
+                if self.normal_term:
+                    return self.acc
+
+                # Swith back if it didn't work out
+                self.input[r][0] = key
 
 if __name__ == "__main__":
     s = solver()
