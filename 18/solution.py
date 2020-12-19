5f175c0f35e09ff8b9b89fefc147fa182e88fddf
tormobr <tormod.brandshoi@gmail.com>
2020-12-19 05:53:12 +0100

    change solution to using regexes to work its way out through the parentethis

--- 18/solution.py
+++ 18/solution.py
@@ -1,93 +1,57 @@
-import math
-from copy import deepcopy
-from collections import deque, defaultdict, Counter
-from functools import reduce
-from operator import mul, add, sub, truediv
-import itertools
-import numpy as np
+from operator import mul, add
 import re
 
 import sys
 sys.path.insert(0,'..')
 from advent_lib import *
 
-directions = {
+operators = {
     "+": add,
-    "-": sub,
-    "*": mul,
-    "/": truediv,
+    "*": mul
 }
 
-dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+# Calculates expression such as 1 + 2 or 4 * 5 and returns the answer
+def sum_add(m):
+    a = int(m["a"])
+    b = int(m["b"])
+    op = operators[m["op"]]
 
+    return str(op(a, b))
+
+# Caclulates the expression inside paranthesis for part 1
+def calculate_inside(m):
+    while "+" in m or "*" in m:
+        m = re.sub("^(?P<a>\d+)\s+(?P<op>.)\s+(?P<b>\d+)", sum_add, m)
+    return m
+
+# Caclulates the expression inside paranthesis for part 2
+def calculate_inside_2(m):
+    while "+" in m:
+        m = re.sub("(?P<a>\d+)\s+(?P<op>\+)\s+(?P<b>\d+)", sum_add, m)
+    while "*" in m:
+        m = re.sub("(?P<a>\d+)\s+(?P<op>\*)\s+(?P<b>\d+)", sum_add, m)
+
+    return m
+
+# iterates over the expression until there are no more paranthesis left
+def iterate(s, f=calculate_inside):
+    inner_par = re.compile(r"\((?P<exp>[^()]+)\)")
+    while "(" in s:
+        s = re.sub(inner_par, lambda m: f(m["exp"]), s)
+
+    return int(f(s))
+    
 
 # Part 1 solution : 
 def part_1():
-    all_data = read_lines("input.txt", f=str)
-    strudel = []
-    for data in all_data:
-        data = re.sub(r"\((:?\d)", r"( \1", data)
-        data = re.sub(r"(:?\d)\)", r"\1 )", data)
-        print(data)
-        data = data.split()
-
-        s = ""
-        hax = []
-        for c in data:
-            if "(" in c:
-                for _ in range(len(c)):
-                    hax.append("(")
-            elif ")" in c:
-                for _ in range(len(c)):
-                    hax.append(")")
-            else:
-                hax.append(c)
-        strudel.append(hax)
-
-
-    def rec(index, hax):
-        print("new recursion")
-        curr_num = None
-        curr_op = None
-        #for j, curr in enumerate(hax[index:]):
-        while index < len(hax):
-            curr = hax[index]
-            print(index, curr)
-            if curr == "(":
-                res, skip = rec(index+1, hax)
-                print("rec op", curr_op, curr_num, res)
-                if curr_num:
-                    curr_num = curr_op(curr_num, res)
-                else:
-                    curr_num = res
-                index = skip
-
-            elif curr == ")":
-                return curr_num, index
-
-            elif curr in ["+", "-", "*", "/"]:
-                curr_op = directions[curr]
-            else:       # number
-                if not curr_num: 
-                    curr_num = int(curr)
-                    print("setting curr_num: ", curr_num)
-                else:
-                    print("norm op:", curr_op, curr_num, curr)
-                    curr_num = curr_op(curr_num, int(curr))
-            index += 1
-        return curr_num, 1000
-
-
-    res = 0
-    for s in strudel:
-        ret,_ = rec(0, s)
-        res += ret
-    return res
+    data = read_lines("input.txt", f=str)
+    return sum(iterate(s) for s in data)
 
 # Part 2 solution : 
 def part_2():
-    return None
-
+    data = read_lines("input.txt", f=str)
+    return sum(iterate(s, f=calculate_inside_2) for s in data)
 
 if __name__ == "__main__":
     pretty_print(part_1(), part_2())
+
