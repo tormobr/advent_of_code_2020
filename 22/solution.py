85d806774862fb375bbdafa48defcfa1782101fa
tormobr <tormod.brandshoi@gmail.com>
2020-12-22 16:45:34 +0100

    day 22 enhanceed

--- 22/solution.py
+++ 22/solution.py
@@ -12,33 +12,22 @@ sys.path.insert(0,'..')
 from advent_lib import *
 
 
+# Gets the final score based on cards and index
 def get_winner_score(a):
-    res = 0
-    for i, x in enumerate(a[::-1]):
-        res += (i+1) * x
-    return res
+    return sum(x * (i+1) for i, x in enumerate(a[::-1]))
 
 # Part 1 solution : 
 def part_1():
     data = open("input.txt").read().strip().split("\n\n")
-    players = []
-    for p in data:
-        s = p.split("\n")
-        players.append(deque([int(c) for c in s[1:]]))
+    player_1, player_2 = [deque(int(c) for c in line.split("\n")[1:]) for line in data]
 
-    player_1 = players[0]
-    player_2 = players[1]
     while True:
-        #print("player 1: ", player_1)
-        #print("player 2: ", player_2)
-        c1 = player_1.popleft()
-        c2 = player_2.popleft()
+        c1, c2 = player_1.popleft(), player_2.popleft()
+
         if c1 > c2:
-            player_1.append(c1)
-            player_1.append(c2)
+            player_1.extend([c1, c2])
         elif c2 > c1:
-            player_2.append(c2)
-            player_2.append(c1)
+            player_2.extend([c2, c1])
         else:
             assert False
 
@@ -54,40 +43,27 @@ def part_1():
 # Part 2 solution : 
 def part_2():
     data = open("input.txt").read().strip().split("\n\n")
-    players = []
-    for p in data:
-        s = p.split("\n")
-        players.append(deque([int(c) for c in s[1:]]))
-
-    player_1 = players[0]
-    player_2 = players[1]
-    SEEN_2 = set()
+    player_1, player_2 = [deque(int(c) for c in line.split("\n")[1:]) for line in data]
+ 
+    # Recursive function to play rounds and games with new rules
     def play(p1, p2, depth):
         SEEN = set()
         while True:
             if tuple(p1) in SEEN:
-                if depth == 0:
-                    return get_winner_score(list(p1))
-                return 0
+                return get_winner_score(list(p1)), 0
             if tuple(p2) in SEEN:
-                if depth == 0:
-                    return get_winner_score(list(p1))
-                return 0
+                return get_winner_score(list(p1)), 0
 
-            SEEN.add(tuple(p1))
-            SEEN.add(tuple(p2))
+            SEEN |= set([tuple(p1), tuple(p2)])
 
 
-            c1 = p1.popleft()
-            c2 = p2.popleft()
+            c1, c2 = p1.popleft(), p2.popleft()
 
             if len(p1) >= c1 and len(p2) >= c2:
-                new_1 = deque(list(p1)[:c1])
-                new_2 = deque(list(p2)[:c2])
-                winner = play(new_1, new_2, depth+1)
+                new_1, new_2 = deque(list(p1)[:c1]), deque(list(p2)[:c2])
+                score, winner = play(new_1, new_2, depth+1)
                 if winner == 0:
                     p1.extend([c1, c2])
-                    #p1.append(c2)
                 elif winner == 1:
                     p2.extend([c2, c1])
                 continue
@@ -100,16 +76,11 @@ def part_2():
 
 
             if len(p1) == 0:
-                if depth == 0:
-                    return get_winner_score(list(p2))
-                return 1
+                return get_winner_score(list(p2)), 1
             elif len(p2) == 0:
-                if depth == 0:
-                    return get_winner_score(list(p1))
-                return 0
+                return get_winner_score(list(p1)), 0
 
-    return play(player_1, player_2, 0)
-    return None
+    return play(player_1, player_2, 0)[0]
 
 
 if __name__ == "__main__":
